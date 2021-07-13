7b2d4ab5adb7971d50df44d29278b46bbe4128d9
Avocado-web <igorahparov@gmail.com>
2021-07-13 22:54:44 +0300

    create: map in hotel section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const hotelSwiper = new Swiper('.hotel-slider', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
